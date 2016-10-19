9bdd199ddb237766321509ad6751f459bfb5df41
Luca Bedogni <lbedogni@cs.unibo.it>
2016-10-19 17:15:32 +0200

    Bugfix

--- ArpaGet/get.py
+++ ArpaGet/get.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 # Usage: ./filename province comma-separated-date-start comma-separated-date-end
 # Example: ./get.py RE 10.10.2016 19.10.2016
-#!/usr/bin/python3
 import urllib.request
 import sys
 try:
@@ -15,10 +15,10 @@ DATE = sys.argv[3]
 # Full string is http://www.arpae.it/estraistaz.asp?q=8&i=19.10.2016&f=19.10.2016&s=3000001&p=RE
 BASE_STR = "http://www.arpae.it/estraistaz.asp?q="
 
-def compose(q):
-    return BASE_STR + str(q) + "&i=" + str(DATS) + "&f=" + str(DATE) + "&s=3000001&p=" + str(PROV)
+def compose(q,stat):
+    return BASE_STR + str(q) + "&i=" + str(DATS) + "&f=" + str(DATE) + "&s=" + str(stat) + "&p=" + str(PROV)
 
-rawdata = BeautifulSoup(urllib.request.urlopen("http://www.arpae.it/v2_rete_di_monitoraggio.asp?p=RE&idlivello=1637").read(), 'html.parser')
+rawdata = BeautifulSoup(urllib.request.urlopen("http://www.arpae.it/v2_rete_di_monitoraggio.asp?p="+str(PROV)+"&idlivello=1637").read(), 'html.parser')
 items = rawdata.find('div', {'id':'dettaglio'}).p.findAll()
 ii = str(items[1]).split("<br>")
 lat = lng = -1
@@ -28,22 +28,41 @@ for item in ii:
     if "Longitudine" in str(item):
         lng = item.split("</b>")[1].strip()
 
-qq = range(10)
-print("Lat is " + str(lat) + ", Lon is " + str(lng))
-for q in qq:
+stations = rawdata.find('select').findAll('option')
+for stat in stations:
+    print("http://www.arpae.it/v2_rete_di_monitoraggio.asp?p="+str(PROV)+"&s="+str(stat.get('value'))+"&idlivello=1637")
     try:
-        rawdata = BeautifulSoup(urllib.request.urlopen(compose(q)).read(), 'html.parser')
-        ii = rawdata.find("tbody").findAll("tr")
+        rawdata = BeautifulSoup(urllib.request.urlopen("http://www.arpae.it/v2_rete_di_monitoraggio.asp?p="+str(PROV)+"&s="+str(stat.get('value'))+"&idlivello=1637").read(), 'html.parser')
+        items = rawdata.find('div', {'id':'dettaglio'}).p.findAll()
     except:
         continue
-    for i in ii:
-        items = i.findAll("td")
+    ii = str(items[1]).split("<br>")
+    lat = lng = -1
+    for item in ii:
+        if "Latitudine" in str(item):
+            lat = item.split("</b>")[1].strip()
+        if "Longitudine" in str(item):
+            lng = item.split("</b>")[1].strip()
+    lat = str(lat).replace(',','.')
+    lng = str(lng).replace(',','.')
 
-        name = str(items[0].text)
-        dates = str(items[1].text)
-        datee = str(items[2].text)
-        instr = str(items[3].text)
-        value = str(items[4].text)
-        unit = str(items[5].text)
+    # qq for now 5 7 8 11
+    qq = range(11)
+    #print("Lat is " + str(lat) + ", Lon is " + str(lng))
+    for q in qq:
+        try:
+            rawdata = BeautifulSoup(urllib.request.urlopen(compose(q,stat.get('value'))).read(), 'html.parser')
+            ii = rawdata.find("tbody").findAll("tr")
+        except:
+            continue
+        for i in ii:
+            items = i.findAll("td")
 
-        print(",".join([name,dates,datee,instr,value,unit]))
+            name = str(items[0].text)
+            dates = str(items[1].text)
+            datee = str(items[2].text)
+            instr = str(items[3].text)
+            value = str(items[4].text)
+            unit = str(items[5].text)
+
+            print(",".join([name,lat,lng,dates,datee,instr,value,unit]))
