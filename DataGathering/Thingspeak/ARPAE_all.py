1d017de252c3e9e833925df0263a7827711a1b72
Federico Montori <fede.selmer@gmail.com>
2017-10-02 15:34:12 +0200

    ARPA new version

---
+++ DataGathering/Thingspeak/ARPAE_all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import time, subprocess, sys
+from datetime import date, timedelta
+
+yesterday = (date.today() - timedelta(1)).strftime('%d.%m.%Y')
+provinces = ["PR", "RE", "PC", "MO", "BO", "FE", "RA", "FC", "RN"]
+#~ provinces = ["BO"]
+
+for PR in provinces:
+    prCmd = ["python3", "getARPAE.py", PR, yesterday, yesterday]
+    prProc = subprocess.Popen(prCmd, stdout=sys.stdout, stderr=sys.stderr)
+    prProc.wait()
+
